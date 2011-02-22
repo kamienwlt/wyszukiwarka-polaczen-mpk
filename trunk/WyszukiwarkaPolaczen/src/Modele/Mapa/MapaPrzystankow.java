fcbd27954fd8da7292326ba9fa4ec6dc849e5c5a
PumpkinHomer@gmail.com <PumpkinHomer@gmail.com@fd8a89c8-757f-5be1-1b20-5e42e9db0f90>
2011-02-22 14:00:03 +0000

    w klasie MapaPrzystankow dopisalem metody do wyszukiwania ścieżki, zmieniłem nazwe paczki "Wagi" na "Modele.Mapa.Wagi"

--- trunk/WyszukiwarkaPolaczen/src/Modele/Mapa/MapaPrzystankow.java
+++ trunk/WyszukiwarkaPolaczen/src/Modele/Mapa/MapaPrzystankow.java
@@ -2,30 +2,33 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Modele.Mapa;
 
-import Wagi.*;
+import Modele.Mapa.Wagi.*;
 import java.io.FileNotFoundException;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Stack;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author x
  */
 public class MapaPrzystankow {
+
     private LinkedList<KrawedzPrzystankow> listaKrawedzi;
     private LinkedList<String> listaWierzch;
     private String sciezka = "data" + File.separator + "MapaPrzystankow";
-
+    HashMap<String, String> p = new HashMap<String, String>();
+    HashMap<String, Integer> d = new HashMap<String, Integer>();
     private WagaCalculatorInterface wagaCalc = new SimpleWagaCalculator();
 
-    public MapaPrzystankow(){
+    public MapaPrzystankow() {
         File plikMapy = new File(sciezka);
         listaKrawedzi = createListaKrawedzi(plikMapy);
         listaWierzch = createListaWierzcholkow(listaKrawedzi);
@@ -36,11 +39,11 @@ public class MapaPrzystankow {
         try {
             Scanner input = new Scanner(plikMapy);
             wynik = new LinkedList<KrawedzPrzystankow>();
-            while(input.hasNextLine()){
+            while (input.hasNextLine()) {
                 String s = input.nextLine();
                 //rozdzielenie pelnych nazw przystankow
                 String[] tab = s.split("#");
-                for(int i = 1; i < tab.length; i++){
+                for (int i = 1; i < tab.length; i++) {
                     //wydzielenie numerow przystankow
                     String poczatek = tab[0].split(" ")[0];
                     String koniec = tab[i].split(" ")[0];
@@ -55,36 +58,43 @@ public class MapaPrzystankow {
         return wynik;
     }
 
-    public void showKrawedzie(){
-        for (KrawedzPrzystankow k : listaKrawedzi){
+    public void showKrawedzie() {
+        for (KrawedzPrzystankow k : listaKrawedzi) {
             k.showKrawedz();
         }
     }
 
-    private void searchPathDijktry(String v0){
+    public void showPathDijkstry(String poczatek, String koniec){
+        calculateDandP(poczatek);
+        Stack<String> s = getPath(koniec);
+        showPath(s);
+    }
+
+    private void calculateDandP(String v0) {
+        System.out.println("Rozpoczeto glowne obliczenia dla przystanku nr " + v0);
         LinkedList<String> S = new LinkedList<String>();
         LinkedList<String> Q = new LinkedList<String>();
-        HashMap<String, String> p = new HashMap<String, String>();
-        HashMap<String, Integer> d = new HashMap<String, Integer>();
+        p = new HashMap<String, String>();
+        d = new HashMap<String, Integer>();
 
-        for(String v : listaWierzch){
+        for (String v : listaWierzch) {
             Q.add(v);
             p.put(v, null);
             d.put(v, Integer.MAX_VALUE);
         }
         d.remove(v0);
         d.put(v0, 0);
-        while(!Q.isEmpty()){
+        while (!Q.isEmpty()) {
             String u = min(Q, d);
             Q.remove(u);
             S.add(u);
-            for (String v : Q){
-                if(krawedzExists(u, v)){
+            for (String v : Q) {
+                if (krawedzExists(u, v)) {
                     int dv = d.get(v);
                     int du = d.get(u);
                     int waga = wagaCalc.getWaga(u, v);
                     int rob = du + waga;
-                    if (dv > rob){
+                    if (dv > rob) {
                         d.remove(v);
                         d.put(v, waga);
                         p.remove(v);
@@ -93,20 +103,43 @@ public class MapaPrzystankow {
                 }
             }
         }
+        System.out.println("Zakonczono glowne obliczenia dla przystanku nr " + v0);
     }
 
-    public static void main(String[] args){
+    private Stack<String> getPath(String i){
+        String rob = i;
+        System.out.println("Rozpoczeto budowanie sciezki do przystanku nr " + rob);
+        Stack<String> wynik = new Stack<String>();
+        while (i != null){
+            wynik.add(i);
+            i = p.get(i);
+        }
+        System.out.println("Zakonczono budowanie sciezki do przystanku nr " + rob);
+        return wynik;
+    }
+
+    private void showPath(Stack<String> s){
+        while(!s.isEmpty()){
+            System.out.println(s.pop());
+        }
+    }
+
+    public static void main(String[] args) {
         MapaPrzystankow m = new MapaPrzystankow();
-        m.showKrawedzie();
+        m.showPathDijkstry("4552", "1421");
     }
 
     private LinkedList<String> createListaWierzcholkow(LinkedList<KrawedzPrzystankow> listaKrawedzi) {
         LinkedList<String> wynik = new LinkedList<String>();
-        for (KrawedzPrzystankow k : listaKrawedzi){
+        for (KrawedzPrzystankow k : listaKrawedzi) {
             String s = k.getKoniec();
-            if(!wynik.contains(s)) wynik.add(s);
+            if (!wynik.contains(s)) {
+                wynik.add(s);
+            }
             s = k.getPoczatek();
-            if(!wynik.contains(s)) wynik.add(s);
+            if (!wynik.contains(s)) {
+                wynik.add(s);
+            }
         }
         return wynik;
     }
@@ -114,9 +147,9 @@ public class MapaPrzystankow {
     private String min(LinkedList<String> Q, HashMap<String, Integer> d) {
         String wynik = "";
         int min = Integer.MAX_VALUE;
-        for(String v : Q){
+        for (String v : Q) {
             int rob = d.get(v);
-            if (rob < min){
+            if (rob <= min) {
                 min = rob;
                 wynik = v;
             }
@@ -127,9 +160,9 @@ public class MapaPrzystankow {
     private boolean krawedzExists(String u, String v) {
         boolean found = false;
         Iterator it = listaKrawedzi.iterator();
-        while(!found && it.hasNext()){
+        while (!found && it.hasNext()) {
             KrawedzPrzystankow k = (KrawedzPrzystankow) it.next();
-            if (k.getPoczatek().equals(u) && k.getKoniec().equals(v)){
+            if (k.getPoczatek().equals(u) && k.getKoniec().equals(v)) {
                 found = true;
             }
         }
